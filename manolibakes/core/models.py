9cae40d7df2680938353b1fb5d6233dc93680dcd
Javier Linares <jlinarestorres2@gmail.com>
2023-10-09 23:39:52 +0200

    Models and first templates

--- manolibakes/core/models.py
+++ manolibakes/core/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name} {self.lastname}"
+
+
+class Bread(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class WeeklyDefaults(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    bread = models.ForeignKey(Bread, on_delete=models.CASCADE)
+    weekday = models.CharField(max_length=20)
+    number = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.customer} necesita {self.number} {self.bread} los {self.weekday}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["customer", "bread", "weekday"], name="unique-weekly-default"
+            )
+        ]
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    bread = models.ForeignKey(Bread, on_delete=models.CASCADE)
+    date = models.DateField()
+    number = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.customer} needs {self.number} {self.bread} on {self.date}"
--- manolibakes/core/views.py
+++ manolibakes/core/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from datetime import date
+from django.shortcuts import render
+import locale
+
+locale.setlocale(locale.LC_TIME, "es_ES.UTF-8")
 
 
-def index(requests):
-    return HttpResponse("Hello world!")
+def index(request):
+    today = date.today().strftime('%A, %d de %B de %Y')
+    context = {
+        "date": today
+    }
+    return render(request, "core/index.html", context)
