a0fc5f913cff1100bc0a8e7c866baa95045f8867
Javier Linares <jlinarestorres2@gmail.com>
2024-07-04 20:16:14 +0200

    remove print statements

--- manolibakes/members/views.py
+++ manolibakes/members/views.py
@@ -6,14 +6,11 @@ from .forms import LogInForm
 
 def login_user(request):
     if request.method == "POST":
-        print("post request!")
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            print("user authenticated!")
             login(request, user)
-            print("user logged in!")
             return redirect("core:index")
     return render(request, "authenticate/login.html", {"date": None, "form": LogInForm})
 
