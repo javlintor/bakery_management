e734d00dbe49931f32ad762c09b29484eb4c2a4f
Javier Linares <jlinarestorres2@gmail.com>
2024-08-16 14:16:42 +0200

    select next day by default

--- manolibakes/core/views.py
+++ manolibakes/core/views.py
@@ -20,13 +20,13 @@ locale.setlocale(locale.LC_TIME, "es_ES.UTF-8")
 
 def get_dates(date_str: str | None = None) -> dict:
     if date_str is None:
-        date = datetime.date.today()
+        date = datetime.date.today() + datetime.timedelta(days=1)
     else:
         try:
             date = datetime.date.fromisoformat(date_str)
         except ValueError:
             logging.info("Incorrent date format.")
-            date = datetime.date.today()
+            date = datetime.date.today() + datetime.timedelta(days=1)
     date_long_str = date.strftime("%A, %d de %B de %Y")
     date_iso_str = date.strftime("%Y-%m-%d")
     return {
