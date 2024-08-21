bdd3242bda493774a8f94703e2889945fb75ee4f
Javier Linares <jlinarestorres2@gmail.com>
2024-08-21 18:32:29 +0200

    search input

--- manolibakes/core/static/core/script.js
+++ manolibakes/core/static/core/script.js
@@ -3,3 +3,21 @@ const dateInputEl = document.getElementById("date-input");
 function dateFormHandler (e) {
     dateInputEl.form.submit();
 }
+
+function filterCustomers() {
+    console.log("filtrando clientes...")
+    var input, filter, customers, li, a, i, txtValue;
+    input = document.getElementById("customerSearchInput");
+    filter = input.value.toUpperCase();
+    customers = document.getElementById("customers");
+    li = customers.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
